4667ad00500e255cc4d7169d0773fdd51f07f42e
somratcste <somratcste@gmail.com>
2018-08-21 16:16:03 +0600

    change author info

--- config/backpack/base.php
+++ config/backpack/base.php
@@ -12,20 +12,20 @@ return [
     */
 
     // Project name. Shown in the breadcrumbs and a few other places.
-    'project_name' => 'Backpack',
+    'project_name' => 'Medicine Info',
 
     // Menu logos
-    'logo_lg'   => '<b>Back</b>pack',
-    'logo_mini' => '<b>B</b>p',
+    'logo_lg'   => '<b>Medicine</b> Info',
+    'logo_mini' => '<b>M</b>I',
 
     // Developer or company name. Shown in footer.
-    'developer_name' => 'Cristian Tabacitu',
+    'developer_name' => 'G. M. Nazmul Hossain',
 
     // Developer website. Link in footer.
     'developer_link' => 'http://tabacitu.ro',
 
     // Show powered by Laravel Backpack in the footer?
-    'show_powered_by' => true,
+    'show_powered_by' => false,
 
     // The AdminLTE skin. Affects menu color and primary/secondary colors used throughout the application.
     'skin' => 'skin-purple',
