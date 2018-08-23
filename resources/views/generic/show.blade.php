25eb732607604c8992150f6d406c88b30dcc661e
somratcste <somratcste@gmail.com>
2018-08-23 13:25:18 +0600

    only show brand name in generic

--- resources/views/generic/show.blade.php
+++ resources/views/generic/show.blade.php
@@ -2,7 +2,7 @@
     <div class="col-md-12">
         <div class="box">
             <div class="box-header with-border">
-                <h3 class="box-title">View All Medicine</h3>
+                <h3 class="box-title">View All Medicine for Generic :: <strong>{{ ucfirst($entity->name) }}</strong></h3>
             </div>
             <!-- /.box-header -->
             <div class="box-body">
@@ -11,14 +11,12 @@
                         <tr>
                             <th>{{ trans('validation.attributes.medicine') }}</th>
                             <th>{{ trans('validation.attributes.brand') }}</th>
-                            <th>{{ trans('validation.attributes.generic') }}</th>
                             <th style="width: 40px">Details</th>
                         </tr>
                     @foreach($products as $key => $product)
                         <tr>
                             <td>{{ ucfirst($product->name) }}</td>
                             <td>{{ ucfirst($product->brand->name) }}</td>
-                            <td>{{ ucfirst($product->generic->name) }}</td>
                             <td><a href="{{ route('search.index', ['type' => 'medicine', 'id' => $product->id]) }}"><span class="badge bg-blue">View</span></a></td>
                         </tr>
                     @endforeach
