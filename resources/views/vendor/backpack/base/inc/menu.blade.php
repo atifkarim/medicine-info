aaa591c513b8004f51c8eea628c1383afe0d7c1e
somratcste <somratcste@gmail.com>
2018-08-24 09:27:28 +0600

    remove registration link

--- resources/views/vendor/backpack/base/inc/menu.blade.php
+++ resources/views/vendor/backpack/base/inc/menu.blade.php
@@ -20,10 +20,10 @@
       <!-- <li><a href="{{ url('/') }}"><i class="fa fa-home"></i> <span>Home</span></a></li> -->
       @if (config('backpack.base.setup_auth_routes'))
         @if (backpack_auth()->guest())
-            <li><a href="{{ url(config('backpack.base.route_prefix', 'admin').'/login') }}">{{ trans('backpack::base.login') }}</a></li>
-            @if (config('backpack.base.registration_open'))
-            <li><a href="{{ route('backpack.auth.register') }}">{{ trans('backpack::base.register') }}</a></li>
-            @endif
+            {{--<li><a href="{{ url(config('backpack.base.route_prefix', 'admin').'/login') }}">{{ trans('backpack::base.login') }}</a></li>--}}
+            {{--@if (config('backpack.base.registration_open'))--}}
+            {{--<li><a href="{{ route('backpack.auth.register') }}">{{ trans('backpack::base.register') }}</a></li>--}}
+            {{--@endif--}}
         @else
             <li><a href="{{ route('backpack.auth.logout') }}"><i class="fa fa-btn fa-sign-out"></i> {{ trans('backpack::base.logout') }}</a></li>
         @endif
